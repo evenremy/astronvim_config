616b958db19ee0efe8e2cfef6fc77eefe4ae7e24
Rémy EVEN <remy.even@ovhcloud.com>
2023-04-26 17:36:51 +0200

    replace pylyze by jedi

--- plugins/mason.lua
+++ plugins/mason.lua
@@ -5,7 +5,7 @@ return {
     "williamboman/mason-lspconfig.nvim",
     -- overrides `require("mason-lspconfig").setup(...)`
     opts = {
-      ensure_installed = { "lua_ls", "pylyzer", "perlnavigator" },
+      ensure_installed = { "lua_ls", "jedi-language-server", "perlnavigator" },
     },
   },
   -- use mason-null-ls to configure Formatters/Linter installation for null-ls sources
